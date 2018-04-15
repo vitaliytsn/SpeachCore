57fd7a7fc84f3810c0a5124c3dc77cab06ac87a0
DESKTOP-POFF5J0\Vital <vitaliytsn@gmail.com>
2018-04-15 13:45:13 +0200

    przerobione i działa z nowym kluczem wciaz nie wiem czemu tak sie zachowuje na serwerze

---
+++ SpeachBingCore/SpeachBingCore/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using SpeachBingCore.Models;
+
+namespace SpeachBingCore.Controllers
+{
+    public class HomeController : Controller
+    {
+        public BingApi ba = new BingApi();
+        public ActionResult Index()
+        {
+            ViewBag.logs = ba._logText;
+            return View();
+        }
+        [HttpPost]
+        public async Task<ActionResult> Index(BingApi ba)
+        {
+
+            List<Task> TaskList = new List<Task>();
+            TaskList.Add(Task.Factory.StartNew(() => ba.StartButton_Click()));
+            ViewBag.logs = "you can start to speak";
+            Task.WaitAll(TaskList.ToArray());
+
+            ViewBag.logs = "you can start to speak";
+            while (ba._logText == null) ;
+            ViewBag.logs = ba._logText;
+            return View("Index");
+        }
+
+    }
+}
